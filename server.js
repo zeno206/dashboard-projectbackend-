e74056a4b09176f4c71dff1b88cb25f773aaa207
shubhamdhiman <shubham.dhiman.mehra@gmail.com>
2026-02-14 13:06:43 +0530

    second-commit

--- server.js
+++ server.js
@@ -1,37 +1,15 @@
-import express from "express";
-import cors from "cors";
-let users = [];
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/api/shubham', (req, res) => {
+    const data = [
+        {name : 'shubham',
+        salary: '40,000',}
+    ]
+  res.send(data)
+})
 
-const app = express();
-const PORT = 5000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ message: "Backend chal raha hai 🚀" });
-});
-
-
-app.post("/api/user", (req, res) => {
-  const data = [{
-    name: "shubham",
-    class: 10,
-    place: "hp",
-  }]
-
-
-
-  res.json(data);
-});
-
-
-app.get("/api/users", (req, res) => {
-  res.json(users);
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
